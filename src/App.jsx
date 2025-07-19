dd7de839bdbe40cfc8e9ed2afba6eb737194a8c8
ATRASH-MULLA <rockyat13@gmail.com>
2025-07-19 19:42:07 +0530

    again

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>JOE MAMA</h1>
+      <h1>ELO SHEEPY :)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
