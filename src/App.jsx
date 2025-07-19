aa20018bf0ddeb3a14a6076e09a6d0b27135d0de
ATRASH-MULLA <rockyat13@gmail.com>
2025-07-19 19:57:46 +0530

    LOL

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Don't edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more NERD !!
       </p>
     </>
   )
