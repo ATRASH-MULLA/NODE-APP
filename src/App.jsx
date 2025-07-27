a92307376a5857e81227aab95851580f1a050dde
ATRASH-MULLA <rockyat13@gmail.com>
2025-07-27 20:39:09 +0530

    edited to test

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ function App() {
 
   return (
     <>
-      <h1>Vite React</h1>
+      <h1>DON'T CLICK ME</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Don't you dare Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more NERD !
       </p>
     </>
   )
